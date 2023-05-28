057f580dc0d5728bbfebc35d063e943af45d2c70
Aless <cristianalessandropopoca@gmail.com>
2023-05-27 21:30:51 -0600

    agregando proyecto

---
+++ index.js
@@ -0,0 +1,52 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "am")
+      .replace(/i/gi, "lo")
+      .replace(/a/gi, "rz")
+      .replace(/o/gi, "popo")
+      .replace(/u/gi, "lzy");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./imagenes/encriptado.jpg";
+    } else {
+      muñeco.src = "./imagenes/munheco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/am/gi, "e")
+      .replace(/lo/gi, "i")
+      .replace(/rz/gi, "a")
+      .replace(/popo/gi, "o")
+      .replace(/lzy/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./imagenes/desencriptado.jpg";
+      } else {
+        muñeco.src = "./imagenes/munheco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
